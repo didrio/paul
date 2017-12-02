7a849e726e381f50b61176b4d7dce0fb1d36d6b4
Andrew Harris <andrewharris@Andrews-MacBook-Pro.local>
2017-12-02 15:37:19 -0800

    added LFO and updated README

--- README.md
+++ README.md
@@ -1,8 +1,175 @@
-# Paul.js
+# About
 
-Paul.js is an abstraction library built on top of the Web Audio API's oscillator and filter features. It's intended to simplify the API and make it easier to work with, especially for people with more of a musical background. 
+Paul.js is an abstraction library built on top of the Web Audio API's oscillator and filter features. It's intended to simplify the API and make it easier to work with, especially for those with a musical background. 
+
+# CDN
+
+```
+CDN coming soon.
+```
+
+# Use
+
+We'll create a new oscillator and start it.
+
+*Osc(note: String, wave: String, octave:Number)*
+```js
+const myOsc = new Osc('C', 'triangle', 4);
+myOsc.play();
+```
+
+We can change certain properties on the oscillator by calling functions on it. 
 
 ```js
-  const osc = new Osc('C', 'triangle', 5);
-  osc.play();
+myOsc.note('A');
+myOsc.wave('saw');
+myOsc.octave(3);
+myOsc.gain(6);
+myOsc.pan(-15);
+```
+
+Gain is within a 0 - 100 range and pan is within a -50 to 50 range (L to R).
+
+Now let's chain these functions together.  
+
+```js
+myOsc.note('C#').wave('sine').octave(6).gain(10).pan(20);
+```
+
+The functions work in any order. 
+
+```js
+myOsc.gain(8).pan(0).wave('square').octave(2).note('eb');
+```
+
+This chaining becomes useful when using the ```to``` and ```over``` functions, which allow us to transition between two oscillator states over the amount of time (in milliseconds) passed into ```over```.
+
+```js
+myOsc.to().note('A').octave(5).over(3000);
+```
+
+```js
+myOsc.gain(0).octave(7).to().note('e').octave(4).gain(6).over(1000);
+```
+
+Let's add a filter! 
+
+*Filter(type: String, frequency: Number, q: Number, detune: Number, gain: Number)*
+```js
+const myFilter = new Filter('highpass', 1200);
+```
+
+Again we can change individual properties on the filter. 
+
+```js
+myFilter.type('highshelf');
+myFilter.frequency(800);
+myFilter.q(10);
+myFilter.detune(100);
+myFilter.gain(-70);
+```
+
+And then we'll add the filter to our oscillator. 
+
+```js
+myOsc.filter(myFilter);
+```
+
+And do a sweep. 
+
+```js
+myFilter.type('lowpass').frequency(100).q(30).to().frequency(20000).q(10).over(5000);
+```
+
+Finally we'll create an LFO.
+
+*LFO(wave: String, frequency: Number, intensity: Number)*
+```js
+const myLfo = new LFO('saw', 10, 200);
+```
+
+Then pass it into our oscillator. 
+```js
+myOsc.lfo(myLfo, 'frequency');
+```
+
+We can also affect the 'gain' and 'pan' of the oscillator with the LFO. And again we can change the LFO's properties. 
+```js
+myLfo.wave('sine');
+myLfo.frequency(5);
+myLfo.intensity(150);
+```
+
+And end. 
+```js
+myOsc.pause();
+```
+
+And now this.
+```js
+const note1 = new Osc('C', 'saw', 5);
+const note2 = new Osc('E', 'saw', 4);
+const note3 = new Osc('G', 'saw', 4);
+const bass = new Osc('F', 'triangle', 1);
+note1.gain(10);
+note2.gain(10);
+note3.gain(10);
+bass.gain(15);
+note1.play();
+note2.play();
+note3.play();
+
+const lowpass = new Filter('lowpass');
+note1.filter(lowpass);
+note2.filter(lowpass);
+note3.filter(lowpass);
+
+lowpass.frequency(200).q(20).to().frequency(20000).q(5).over(15000);
+
+setTimeout(() => {
+  note1.to().note('E').over(400);
+  note2.to().note('B').over(400);
+  note3.to().note('Ab').over(400);
+}, 1600);
+
+setTimeout(() => {
+  note1.to().note('A').octave(4).over(400);
+  note2.to().note('C').over(400);
+  note3.to().note('E').over(400);
+}, 3600);
+
+setTimeout(() => {
+  note1.to().note('A').octave(4).over(400);
+  note2.to().note('F').pan(-10).over(400);
+  note3.to().note('C').pan(10).over(400);
+}, 5600);
+
+setTimeout(() => {
+  note1.to().note('C').over(400);
+  note2.to().note('E').pan(-20).over(400);
+  note3.to().note('G').pan(20).over(400);
+  bass.play();
+  bass.to().note('C').octave(2).over(200);
+}, 7600);
+
+setTimeout(() => {
+  note1.to().note('E').over(400);
+  note2.to().note('B').pan(-30).over(400);
+  note3.to().note('Ab').pan(30).over(400);
+  bass.to().note('E').over(200);
+}, 9600);
+
+setTimeout(() => {
+  note1.to().note('A').octave(4).over(400);
+  note2.to().note('C').pan(-40).over(400);
+  note3.to().note('E').pan(40).over(400);
+  bass.to().note('A').over(200);
+}, 11600);
+
+setTimeout(() => {
+  note1.to().note('A').octave(4).over(400);
+  note2.to().note('F').pan(-50).over(400);
+  note3.to().note('C').pan(50).over(400);
+  bass.to().note('F').over(200);
+}, 13600);
 ```
--- paul.js
+++ paul.js
@@ -1,33 +1,133 @@
 const paulAudioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-function Filter(type = 'lowpass', frequency = 800, q = 0, detune = 0, gain = 10) {
+function LFO(wave, frequency, intensity) {
+  if (wave === 'saw') wave = 'sawtooth';
+  this.oscillator = new OscillatorNode(paulAudioCtx);
+  this.gainNode = new GainNode(paulAudioCtx);
+  this.oscillator.frequency.value = frequency;
+  this.oscillator.type = wave;
+  this.gainNode.gain.value = intensity;
+  this.oscillator.connect(this.gainNode);
+}
+
+LFO.prototype.wave = function(wave) {
+  if (wave === 'saw') wave = 'sawtooth';
+  this.oscillator.type = wave;
+  return this;
+}
+
+LFO.prototype.frequency = function (frequency) {
+  this.oscillator.frequency.value = frequency;
+  return this;
+}
+
+LFO.prototype.intensity = function (intensity) {
+  this.gainNode.gain.value = intensity;
+  return this;
+}
+
+function Filter(type = 'lowpass', frequency = 800, q = 0, detune = 0, gain = 0) {
+  if (gain < -100) gain = -100;
+  if (gain > 100) gain = 100;
+  gain = (gain / 100) * 40;
   this.props = { type, frequency, q, detune, gain };
   this.filters = [];
+}
+
+Filter.prototype.to = function () {
+  this.mod = {};
+  return this;
+}
+
+Filter.prototype.over = function (ms) {
+  if (this.interval) clearInterval(this.interval);
+  let intervals = 0;
+  const inveralMs = 20;
+  const targetIntervals = ms / inveralMs;
+  let freqPerInterval, qPerInterval, detunePerInterval, gainPerInterval;
+  //FREQUENCY
+  if (this.mod.frequency !== undefined) {
+    const freqDifference = -(this.props.frequency - this.mod.frequency);
+    freqPerInterval = (freqDifference / targetIntervals);
+  }
+  //Q
+  if (this.mod.q !== undefined) {
+    const qDifference = -(this.props.q - this.mod.q);
+    qPerInterval = (qDifference / targetIntervals);
+  }
+  //DETUNE
+  if (this.mod.detune !== undefined) {
+    const detuneDifference = -(this.props.detune - this.mod.detune);
+    detunePerInterval = (detuneDifference / targetIntervals);
+  }
+  //GAIN
+  if (this.mod.gain !== undefined) {
+    const gainDifference = -(this.props.gain - this.mod.gain);
+    gainPerInterval = (gainDifference / targetIntervals);
+  }
+  //INTERVAL
+  this.interval = setInterval(() => {
+    intervals++;
+    if (intervals === targetIntervals) {
+      clearInterval(this.interval);
+      this.mod = null;
+    }
+    this.filters.forEach(filter => {
+      if (freqPerInterval) filter.frequency.value += freqPerInterval;
+      if (qPerInterval) filter.Q.value += qPerInterval;
+      if (detunePerInterval) filter.detune.value += detunePerInterval;
+      if (gainPerInterval) filter.gain.value += gainPerInterval;
+    });
+  }, inveralMs);
 }
 
 Filter.prototype.type = function(type) {
   this.props.type = type;
   this.filters.forEach(filter => filter.type = type);
+  return this;
 }
 
 Filter.prototype.frequency = function (frequency) {
+  if (this.mod) {
+    this.mod.frequency = frequency;
+    return this;
+  }
   this.props.frequency = frequency;
   this.filters.forEach(filter => filter.frequency.value = frequency);
+  return this;
 }
 
 Filter.prototype.q = function (q) {
+  if (this.mod) {
+    this.mod.q = q;
+    return this;
+  }
   this.props.q = q;
   this.filters.forEach(filter => filter.Q.value = q);
+  return this;
 }
 
 Filter.prototype.detune = function (detune) {
+  if (this.mod) {
+    this.mod.detune = detune;
+    return this;
+  }
   this.props.detune = detune;
   this.filters.forEach(filter => filter.detune.value = detune);
+  return this;
 }
 
 Filter.prototype.gain = function (gain) {
+  if (this.mod) {
+    this.mod.gain = gain;
+    return this;
+  }
+  if (gain < -100) gain = -100;
+  if (gain > 100) gain = 100;
+  gain = (gain / 100) * 40;
   this.props.gain = gain;
   this.filters.forEach(filter => filter.gain.value = gain);
+  return this;
 }
 
 function Osc(note, wave = 'sine', octave = 4) {
@@ -116,38 +216,55 @@ Osc.prototype.gain = function(gain) {
 Osc.prototype.note = function(note) {
   let frequency;
   switch (note) {
+    case 'c':
     case 'C': frequency = 261.63;
       break;
+    case 'c#':
     case 'C#': frequency = 277.18;
       break;
+    case 'db':
     case 'Db': frequency = 277.18;
       break;
+    case 'd':
     case 'D': frequency = 293.66;
       break;
+    case 'd#':
     case 'D#': frequency = 311.13;
       break;
+    case 'eb':
     case 'Eb': frequency = 311.13;
       break;
+    case 'e':
     case 'E': frequency = 329.63;
       break;
+    case 'f':
     case 'F': frequency = 349.23;
       break;
+    case 'f#':
     case 'F#': frequency = 369.99;
       break;
+    case 'gb':
     case 'Gb': frequency = 369.99;
       break;
+    case 'g':
     case 'G': frequency = 392.00;
       break;
+    case 'g#':
     case 'G#': frequency = 415.30;
       break;
+    case 'ab':
     case 'Ab': frequency = 415.30;
       break;
+    case 'a':
     case 'A': frequency = 440.00;
       break;
+    case 'a#':
     case 'A#': frequency = 466.16;
       break;
+    case 'bb':
     case 'Bb': frequency = 466.16;
       break;
+    case 'b':
     case 'B': frequency = 493.88;
       break;
     default: frequency = 261.63;
@@ -196,9 +313,15 @@ Osc.prototype.octave = function(octave) {
   return this;
 }
 
+Osc.prototype.wave = function (wave) {
+  if (wave === 'saw') wave = 'sawtooth';
+  this.oscillator.type = wave;
+  return this;
+}
+
 Osc.prototype.play = function () {
   if (this.muted) {
-    this.gainNode.connect(paulAudioCtx.destination);
+    this.lastNode.connect(paulAudioCtx.destination);
     this.muted = false;
   } else this.oscillator.start();
   return this;
@@ -206,13 +329,13 @@ Osc.prototype.play = function () {
 
 Osc.prototype.pause = function () {
   if (!this.muted) {
-    this.gainNode.disconnect(paulAudioCtx.destination);
+    this.lastNode.disconnect(paulAudioCtx.destination);
     this.muted = true;
   }
   return this;
 }
 
-Osc.prototype.add = function (filterNode) {
+Osc.prototype.filter = function (filterNode) {
   const filter = new BiquadFilterNode(paulAudioCtx);
   filter.type = filterNode.props.type;
   filter.frequency.value = filterNode.props.frequency;
@@ -226,9 +349,25 @@ Osc.prototype.add = function (filterNode) {
   filterNode.filters.push(filter);
 }
 
-Osc.prototype.wave = function (wave) {
-  if (wave === 'saw') wave = 'sawtooth';
-  this.wave = wave;
-  this.oscillator.type = wave;
-  return this;
+Osc.prototype.lfo = function (lfo, destination) {
+  switch (destination) {
+    case 'frequency': 
+      lfo.gainNode.connect(this.oscillator.frequency);
+      lfo.oscillator.start();
+      break;
+    case 'gain':
+      if (lfo.gainNode.gain.value > 100) lfo.gainNode.gain.value = 100;
+      if (lfo.gainNode.gain.value < 0) lfo.gainNode.gain.value = 0;
+      lfo.gainNode.gain.value = lfo.gainNode.gain.value / 100;
+      lfo.gainNode.connect(this.gainNode.gain);
+      lfo.oscillator.start();
+      break;
+    case 'pan':
+      if (lfo.gainNode.gain.value < -50) lfo.gainNode.gain.value = -50;
+      else if (lfo.gainNode.gain.value > 50) lfo.gainNode.gain.value = 50;
+      lfo.gainNode.gain.value = lfo.gainNode.gain.value / 50;
+      lfo.gainNode.connect(this.pannerNode.pan);
+      lfo.oscillator.start();
+      break;
+  }
 }
